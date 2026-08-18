411fd3e47ddd1975520ac71b0c568feb0dbf4cba
Meet-1308 <du3802008@gmail.com>
2026-08-18 20:17:30 +0530

    Create Lab5_6.java

---
+++ Lab5_6.java
@@ -0,0 +1,16 @@
+public class lab5_6 {
+    public static void main(String[] args) {
+        int size = 5; 
+
+        for (int i = 0; i <= size; i++) {
+            for (int j = 0;j <= size; j++) {
+                if (i == 0 || i == size || j == 0 || j == size) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  "); 
+                }
+            }
+            System.out.println();
+        }
+    }
+}
